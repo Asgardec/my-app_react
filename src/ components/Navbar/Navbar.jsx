223f9793eee5bffb8817534dd6adb02fbe5d953f
Asgardec <andrew.ya77@gmail.com>
2021-02-10 16:58:13 +0300

    delete readme and rename

--- src/ components/Navbar/Navbar.jsx
+++ src/ components/Navbar/Navbar.jsx
@@ -1,25 +1,24 @@
-
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 const Navbar = () => {
-return <nav className={s.nav}>
-  <div className={s.item}>
-    <a href="/Profile">Profile</a>
-  </div >
-  <div className={s.item}>
-    <a href="/Dialogs">Messages</a>
-  </div>
-  <div className={s.item}>
-    <a>News</a>
-  </div>
-  <div className={s.item}>
-    <a>Music</a>
-  </div>
-  <div className={s.item}>
-    <a>Settings</a>
-  </div>
-</nav >
-
+  return (<nav className={s.nav}>
+    <div className={s.item}>
+      <NavLink to="/Profile" activeClassName={s.active}>Profile</NavLink>
+    </div >
+    <div className={`${s.item} ${s.active}`}>
+      <NavLink to="/Dialogs" activeClassName={s.active}>Messages</NavLink>
+    </div>
+    <div className={s.item}>
+      <NavLink to="/News" activeClassName={s.active}>News</NavLink>
+    </div>
+    <div className={s.item}>
+      <NavLink to="Music" activeClassName={s.active}>Music</NavLink>
+    </div>
+    <div className={s.item}>
+      <NavLink to="Settings" activeClassName={s.active}>Settings</NavLink>
+    </div>
+  </nav >);
 }
 
 export default Navbar;
--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
+
+import {BrowserRouter, Route} from "react-router-dom";
 import React from 'react';
+
 import Dialogs from './ components/Dialogs/Dialogs';
 import Header from './ components/Header/Header';
+import Music from './ components/Music/Music';
 import Navbar from './ components/Navbar/Navbar';
-
-//import Profile from './ components/Profile/Profile';
+import News from './ components/News/News';
+import Profile from './ components/Profile/Profile';
+import Settings from './ components/Settings/Settings';
 
 
 const App = () => {
-  return (<div className="app-wrapper">
+  return (
+    <BrowserRouter>
+    <div className="app-wrapper">
     <Header />
     <Navbar />
     <div class='app-wrapper-content'>
-    <Dialogs />
+      <Route path="/dialogs" component={Dialogs} />
+      <Route path="/Profile" component={Profile} />
+      <Route path="/News" component={News} />
+      <Route path="/Music" component={Music} />
+      <Route path="/Settings" component={Settings} />
     </div>
-    {/*<Profile />*/}
-  </div>)
+  </div>
+</BrowserRouter>)
 }
 
 export default App;
